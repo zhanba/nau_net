1095aa817dd59034ab10abc5e6656f2f54808cb5
zhanba <c5e1856@gmail.com>
2017-02-22 13:25:28 +0800

    modified setup.py

--- setup.py
+++ setup.py
@@ -1,28 +1,29 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='nau_net',
     version='1.0.0.dev1',
     description='NAU school network CLI.',
-    url='https://github.com/calvinjiang/hive-executor-py',
+    url='https://github.com/zhanba/nau_net',
     author='zhanba',
-    author_email='c5e1856@163.com',
+    author_email='c5e1856@gmail.com',
     license='MIT',
     classifiers=[
         'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Build Tools',
+        'Intended Audience :: End Users/Desktop',
+        'Topic :: Internet :: WWW/HTTP',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
-    keywords='hive client python',
-    packages=['hive'],
+    keywords='NAU network CLI',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+    install_requires=['click', 'requests', 'beautifulsoup4'],
+    entry_points={
+        'console_scripts': [
+            'nau = nau.cli:cli',
+        ],
+    }
 )
